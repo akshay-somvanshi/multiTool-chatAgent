7b779bd6f949512b8ccad4655d8cb8fcb40d7903
akshay-somvanshi <akshaysomvanshi18@gmail.com>
2025-11-24 13:44:07 +0100

    Working grounding

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from google import genai
 from langchain_google_vertexai import HarmBlockThreshold, HarmCategory
 from langchain_google_vertexai import ChatVertexAI
 from langchain_google_community import GoogleSearchAPIWrapper
@@ -16,10 +15,6 @@ location = 'europe-west1'
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 GOOGLE_CSE_ID = os.getenv('GOOGLE_CSE_ID')
 
-# client_gemini = genai.Client(
-#     vertexai=True, project=project_id, location=location
-# )
-
 basic_model = "gemini-2.5-flash"
 advanced_model = "gemini-2.5-pro"
 
@@ -32,6 +27,13 @@ safety_settings = {
     HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
 }
 
+system_instruction = (
+    "You are a helpful assistant. Always use the 'google_search' tool for any questions "
+    "related to current events, future events, or any information that changes over time "
+    "such as news, weather, or real-time facts. Only answer from your internal knowledge "
+    "if the information is general or timeless (e.g., historical facts). Start every answer with 'Doh!' "
+)
+
 model_kwargs = {
     # Temperature - degree of randomness
     "temperature": 1.0, 
@@ -41,22 +43,16 @@ model_kwargs = {
     "top_p": 0.95,
     # Top k - next token selected among top-k 
     "top_k": None,
-    "safety_settings": safety_settings
+    "safety_settings": safety_settings,
+    "system_instruction": system_instruction,
 }
-
-search_wrapper = GoogleSearchAPIWrapper()
-google_search_tool = Tool(
-    name="google_search",
-    description="Search Google for recent results.",
-    func=search_wrapper.run,
-)
 
 @tool
 def search_web(query: str):
     """Search the web"""
     return f"Search result placeholder for: {query}"
 
-tools_list = [google_search_tool]
+tools_list = []
 
 # Enable switching to pro model 
 @wrap_model_call
@@ -71,20 +67,32 @@ def model_selection(request: ModelRequest, handler):
     else:
         print(f"Selecting Basic Model ({basic_model})")
         model_name = basic_model
-
+    
+    # Bind model to google search
     model = ChatVertexAI(
         model_name=model_name,
-        temperature=1.0,
-        max_output_tokens=1000,
-        top_p=0.95,
-        safety_settings=safety_settings
-    )
+        temperature=model_kwargs.get('temperature'),
+        max_tokens=model_kwargs.get('max_output_tokens'),
+        top_p=model_kwargs.get('top_p'),
+        top_k=model_kwargs.get('top_k'),
+        # safety_settings=model_kwargs.get('safety_settings'),
+    ).bind_tools([{"google_search": {}}])
 
     return handler(request.override(model=model))
 
+# Bind model to google search
+llm = ChatVertexAI(
+    model_name=basic_model,
+    temperature=model_kwargs.get('temperature'),
+    max_tokens=model_kwargs.get('max_output_tokens'),
+    top_p=model_kwargs.get('top_p'),
+    top_k=model_kwargs.get('top_k'),
+    # safety_settings=model_kwargs.get('safety_settings'),
+).bind_tools([{"google_search": {}}])
+
 # Create agent
 agent = create_agent(
-    basic_model, 
+    llm,
     tools=tools_list,
     middleware=[model_selection])
 
